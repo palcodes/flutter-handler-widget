0778f15879b04a6b6c87ed266d65babb20bb460d
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-10-26 02:29:25 +0530

    added animation code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,21 +27,66 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+
+  double minHeight = 50;
+  double maxHeight = 120;
+  double initHeight = 20;
+  AnimationController _controller;
+  Animation<double> widthTween;
+  Animation<double> bottomTween;
+  double get maxWidth => MediaQuery.of(context).size.width;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  double lerp(double min, double max) => lerpDouble(min, max, _controller.value);
 
   @override
   Widget build(BuildContext context) {
+    widthTween = Tween(begin: 120.0, end: MediaQuery.of(context).size.width).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linearToEaseOut,
+        reverseCurve: Curves.linearToEaseOut.flipped,
+      ));
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Container(
-        alignment: Alignment.center,
-        child: FlatButton(
-          onPressed: () {
-            print('clicked the button');
-          },
-          child: Icon(Icons.attach_file),
-        ),
-      ),
-    );
+        appBar: AppBar(title: Text(widget.title)),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child: FlatButton(
+                onPressed: () {
+                  _controller.forward();
+                },
+                child: Icon(Icons.attach_file),
+              ),
+            ),
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Container(
+              color: Colors.black,
+              height: 20,
+              width: 20,
+                );
+              },
+            )
+          ],
+        ));
   }
 }
