c3776a27b9184bccc119da50889dc14c7fd235d6
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-10-26 03:31:45 +0530

    animation works now, need work on finishing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
@@ -29,22 +30,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-
-  double minHeight = 50;
-  double maxHeight = 120;
-  double initHeight = 20;
+  double minHeight = 1;
+  double maxHeight = 200;
+  double initHeight = 1;
   AnimationController _controller;
   Animation<double> widthTween;
   Animation<double> bottomTween;
   double get maxWidth => MediaQuery.of(context).size.width;
 
+  bool animationGoingOn;
+  bool animationOver;
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(seconds: 10),
+      duration: const Duration(seconds: 2),
       vsync: this,
-    )..repeat();
+    );
   }
 
   @override
@@ -53,15 +56,17 @@ class _MyHomePageState extends State<MyHomePage>
     super.dispose();
   }
 
-  double lerp(double min, double max) => lerpDouble(min, max, _controller.value);
+  double lerp(double min, double max) =>
+      lerpDouble(min, max, _controller.value);
 
   @override
   Widget build(BuildContext context) {
-    widthTween = Tween(begin: 120.0, end: MediaQuery.of(context).size.width).animate(CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linearToEaseOut,
-        reverseCurve: Curves.linearToEaseOut.flipped,
-      ));
+    widthTween = Tween(begin: 120.0, end: MediaQuery.of(context).size.width)
+        .animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+      reverseCurve: Curves.linearToEaseOut.flipped,
+    ));
     return Scaffold(
         appBar: AppBar(title: Text(widget.title)),
         body: Column(
@@ -79,14 +84,57 @@ class _MyHomePageState extends State<MyHomePage>
             AnimatedBuilder(
               animation: _controller,
               builder: (context, child) {
-                return Container(
-              color: Colors.black,
-              height: 20,
-              width: 20,
+                return Stack(
+                  children: <Widget>[
+                    Container(
+                      height: lerp(minHeight, maxHeight),
+                      width: widthTween.value,
+                      color: Colors.grey[200],
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 65),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          MaterialButton(   
+                            padding: EdgeInsets.all(12.0),
+                            child: new Icon(Icons.file_upload, color: Colors.white, size: 28.0),
+                            onPressed: () {},
+                            shape: new CircleBorder(),
+                            elevation: 2.0,
+                            color: Colors.black,
+                          ),
+                           MaterialButton(
+                            padding: EdgeInsets.all(12.0),
+                            child: new Icon(Icons.image, color: Colors.white, size: 28.0),
+                            onPressed: () {},
+                            shape: new CircleBorder(),
+                            elevation: 2.0,
+                            color: Colors.black,
+                          ),
+                           MaterialButton(
+                            padding: EdgeInsets.all(12.0),
+                            child: new Icon(Icons.videocam, color: Colors.white, size: 28.0),
+                            onPressed: () {},
+                            shape: new CircleBorder(),
+                            elevation: 2.0,
+                            color: Colors.black,
+                          ),
+                      ],)
+                    ),
+                  ],
                 );
               },
             )
           ],
         ));
+  }
+
+  void handleAnimation() {
+    //need the controller to show or [visible=true] the three overlaying stacked buttons while animation  
+  }
+
+  void afterAnimationOver() {
+    //need to wait for another click on the attach_file button, so when it is clicked, it can roll back the animation & all the buttons will hide
   }
 }
